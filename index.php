f9be26c5f9dd3cf9a9f0768d05b3e0526667bff3
SirTapir <37269469+SirTapir@users.noreply.github.com>
2018-06-15 20:42:57 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+	require_once "connect.php";
+	$sql = "SELECT * FROM products WHERE featured=1";
+	$result = $conn->query($sql);
+	$result2 = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -48,61 +54,82 @@
 	<div class="col-md-10">
 		<div class="row">
 			<h2 class="text-center">Featured Products</h2>
+			<?php while($product = mysqli_fetch_assoc($result)) : ?>
 			<div class="col-md-3">
-				<h4>Keyboard</h4>
-				<img src="images/product1.png" alt="Keyboard" id="images">
-				<p class="price">Price: Rp1.550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-1">Details</button>
+				<h4><?=$product['title']; ?></h4>
+				<img src="<?=$product['image']; ?>" alt="<?=$product['title']; ?>" id="images">
+				<p class="price">Price: Rp<?=$product['price']; ?></p>
+				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-<?=$product['id']; ?>">Details</button>
 			</div>
 
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
-			</div>
+		<?php endwhile; ?>
 
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
-			</div>
-
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
-			</div>
-
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
-			</div>
-
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
-			</div>
-
-			<div class="col-md-3">
-				<h4>Mouse</h4>
-				<img src="images/product2.png" alt="Mouse" id="images">
-				<p class="price">Price: Rp550.000</p>
-				<button type="button" class="btn btn-success" data-toggle="modal" data-target="#details-2">Details</button>
+			
+		</div>
+	<!--details modal-->
+	<?php while($product = mysqli_fetch_assoc($result2)) : ?>
+		<div class="modal fade details-1" id="details-<?=$product['id']; ?>" tableindex="-1" role="dialog" >
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h4 class="modal-title text-center"><?=$product['title']; ?></h4>
+					</div>
+					<div class="modal-body">
+						<div class="container-fluid">
+							<div class="row">
+								<div class="col-sm-6">
+									<div class="center-block">
+										<img src="<?=$product['image']; ?>" alt="<?=$product['title']; ?>" class="details img-responsive">
+									</div>
+								</div>
+								<div class="col-sm-6">
+									<h4>Details</h4>
+									<p><?=$product['description']; ?></p>
+									<hr />
+									<p>Price : Rp <?=$product['price']; ?></p>
+									<p>Brand : <?php 
+													$sql2 = "SELECT * FROM brands WHERE id=".$product['brand'];
+													$brandArray = $conn->query($sql2);
+													while($brandName = mysqli_fetch_assoc($brandArray)) :
+														echo $brandName['brand'];
+													endwhile;
+												 ?> </p>
+									<form action="add_cart.php" method="post">
+										<div class="form-group">
+											<div class="col-xs-3">
+												<label for="quantity" id="quantity-label">Quantity</label>
+												<input type="text" class="form-control" id="quantity" name="quantity">
+											</div><br><br>
+											<div class="form-group">
+												<label for="LED Color">Color</label>
+												<select name="ledColor" id="ledColor" class="form-control">
+													<option value=""></option>
+													<?php
+														$color=explode(',', $product['color']);
+														foreach ($color as $eachColor) {
+															echo "<option value=' ".$eachColor."'>".$eachColor."</option>";
+														}
+													?>
+													
+												</select>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="modal-footer">
+							<button class="btn btn-default" data-dismiss="modal">Close</button>
+							<button class="btn btn-warning" type="submit"> <span class='glyphicon glyphicon-shopping-cart'>Add To Cart</span></button>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
-	
-	</div>
-	<!--details modal-->
-	<?php
-		include "detail-modal-product1.php";
-	?>
+	<?php endwhile; ?>	
 	<!--end detail modal-->
 </body>
 </html>
