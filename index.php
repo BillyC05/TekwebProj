3d5c4f9e5b4ff504c5ace489d35371dd2880e20b
BillyC05 <37071331+BillyC05@users.noreply.github.com>
2018-06-17 19:06:33 +0700

    Add Carousel
    Added Carousel (3 items, no link yet)

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
         <div class="navbar-header">
           <a class="navbar-brand" href="#page-top">OnlineShop</a>
         </div>
-        <form class="navbar-form navbar-left" action="#search#">
+        <form class="navbar-form navbar-left" action="#search#" style="width:700px;">
           <div class="input-group">
             <input type="text" class="form-control" placeholder="Search Online Shop" id="navSearchBar"/>
             <div class="input-group-btn">
@@ -45,7 +45,39 @@
     </nav>
 
 		<!-- Carousel -->
+		<div class="carousel slide" data-slide="carousel" id="carousel-1">
+			<!-- Indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-1" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-1" data-slide-to="1"></li>
+				<li data-target="#carousel-1" data-slide-to="2"></li>
+			</ol>
 
+			<!-- Slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="images/product2.png" alt="Item #1" style="height:100% !important; margin:auto;">
+				</div>
+				<div class="item">
+					<img src="images/product2.png" alt="Item #2" style="height:100% !important; margin:auto;">
+				</div>
+				<div class="item">
+					<img src="images/product2.png" alt="Item #3" style="height:100%!important; margin:auto;">
+				</div>
+			</div>
+
+			<!-- Controls -->
+			<a class="left carousel-control" href="#carousel-1" data-slide="prev" style="background-image: none;">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a href="#carousel-1" class="right carousel-control" data-slide="next" style="background-image: none;">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+
+		<!-- Carousel end -->
 
 		<!-- Centre -->
 		<div class="col-md-1"></div>
@@ -111,6 +143,7 @@
 			include "detail-modal-product1.php";
 		?>
 		<!--end detail modal-->
+
 
 
   </body>
